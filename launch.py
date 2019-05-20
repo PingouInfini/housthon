85594f7346acd8535b20b5d0b5a21734fb6572b3
peploleum <peploleum@gmail.com>
2019-05-20 15:06:37 +0200

    interface REST pour traiter 94A_json, v0

--- launch.py
+++ launch.py
@@ -1,2 +1,21 @@
+import sys
+import threading
+from flask import Flask
+from flask import request
+
+custom_port = 8090
+app = Flask(__name__)
+
+@app.route('/start_process94A', methods=['POST'])
+def prepare_94A():
+    habilitation_json = request.get_json()
+    nomfamille = habilitation_json['94A']['nom de famille']
+    prenom = habilitation_json['94A']['prenom'][1]
+    return prenom
+
+def start_REST_server(port):
+    app.run(host='0.0.0.0', port=port)
+
+
 if __name__ == '__main__':
-    print ("bienvenue dans Housthon, décollage immédiat!")
+    app.run(host='0.0.0.0', port=custom_port)
