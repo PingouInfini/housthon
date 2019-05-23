c33beab50d5a7f05e124f9d84090e3a41516d194
peploleum <peploleum@gmail.com>
2019-05-23 17:16:30 +0200

    Modification pour docker

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 rm -rf ./docker/context/dockerdist/*
 touch ./docker/context/dockerdist/README.md
+mkdir -p ./docker/context/dockerdist/src/ && cp -Rf ./src/* ./docker/context/dockerdist/src/
 cp -Rf ./housthon.iml ./docker/context/dockerdist/
 cp -Rf ./launch.py ./docker/context/dockerdist/
 cp -Rf ./setup.py ./docker/context/dockerdist/
