ef2e8b7019c4e3550cd0df274c46f1c7253e3cdd
peploleum <peploleum@gmail.com>
2019-05-24 17:37:15 +0200

    Ajout de l'envoi des vacances de conjoint dans travelthon

--- README.md
+++ README.md
@@ -12,8 +12,16 @@ COLISSITHON_IP >ip de la brique collissithon
 COLISSITHON_PORT ->port de la brique collissithon
 HOUSTHON_PORT ->port pour le service rest start_process94A
 
+TOPIC_GOOGLETHON : topic kafka pour googlethon
+TOPIC_TWITTHON: topic kafka pour twitthon
+TOPIC_TRAVELTHON: topic kafka pour travelthon
+TOPIC_COMPARATHON: topic kafka pour comparathon
+
 adresse du service REST
-http://localhost:HOUSTHON_PORT/start_process94A
+http://ip_machine:HOUSTHON_PORT/start_process94A
 
 recupere 'nom de famille', 1er 'prenom', 'photo' dans un json 94A
-pour creer un bioGraphics dans insight via le service create_bio dans collissithon
+pour creation d'un bioGraphics dans insight via le service create_bio dans collissithon
+
+creation bioGraphics du pere, de la mere, du conjoint et beaux parents
+relation entre candidat et  pere,  mere,  conjoint et beaux parents
--- launch.py
+++ launch.py
@@ -13,22 +13,22 @@ from kafka import KafkaProducer
 app = Flask(__name__)
 
 #pour recupere variable d'env du yml
-housthon_port=os.environ['HOUSTHON_PORT']
-colissithon_url_port="http://"+str(os.environ['COLISSITHON_IP'])+":"+str(os.environ['COLISSITHON_PORT'])
-kafka_endpoint = str(os.environ['KAFKA_IP']) + ":" + str(os.environ['KAFKA_PORT'])
-comparathon_in=str(os.environ['TOPIC_GOOGLETHON'])
-tweethon_in=str(os.environ['TOPIC_TWITTHON'])
-googlethon_in=os.environ['TOPIC_GOOGLETHON']
-travelthon_in=os.environ['TOPIC_TRAVELTHON']
+#housthon_port=os.environ['HOUSTHON_PORT']
+#colissithon_url_port="http://"+str(os.environ['COLISSITHON_IP'])+":"+str(os.environ['COLISSITHON_PORT'])
+#kafka_endpoint = str(os.environ['KAFKA_IP']) + ":" + str(os.environ['KAFKA_PORT'])
+#comparathon_in=str(os.environ['TOPIC_GOOGLETHON'])
+#tweethon_in=str(os.environ['TOPIC_TWITTHON'])
+#googlethon_in=os.environ['TOPIC_GOOGLETHON']
+#travelthon_in=os.environ['TOPIC_TRAVELTHON']
 
 #pour tester sur poste de dev
-#housthon_port=8090
-#colissithon_url_port="http://192.168.0.13:9876"
-#kafka_endpoint =  "192.168.0.13:8092"
-#comparathon_in="comparathon_in"
-#tweethon_in="tweethon_in"
-#googlethon_in="googlethon_in"
-#travelthon_in="travelthon_in"
+housthon_port=8090
+colissithon_url_port="http://192.168.0.13:9876"
+kafka_endpoint =  "192.168.0.13:8092"
+comparathon_in="comparathon_in"
+tweethon_in="tweethon_in"
+googlethon_in="googlethon_in"
+travelthon_in="travelthon_in"
 
 hostname = socket.gethostname()
 ip = socket.gethostbyname(hostname)
@@ -64,9 +64,11 @@ def process_94A():
 
     #recuperation des voyages
     voyage_json=habilitation_json['94A']['Voyages depuis 5 ans']
+    voyage_conjoint_json=habilitation_json['94A']['Conjoint']['Voyages depuis 5 ans']
 
     #creation du candidat
     idbio=services.create_bio_minibio(prenom, nomfamille, image, typeimage, colissithon_url_port)
+    #idbio="1234567890"
     #creation du pere
     idbio_pere=services.create_bio_minibio(prenom_pere, nom_famille_pere, None, None,  colissithon_url_port)
     #relation entre les deux id
@@ -76,6 +78,7 @@ def process_94A():
     services.bind_bio_colissithon(idbio,idbio_mere, colissithon_url_port)
     #creation du conjoint
     idbio_conjoint=services.create_bio_minibio(prenom_conjoint, nom_famille_conjoint, None, None,  colissithon_url_port)
+    #idbio_conjoint="0987654321"
     services.bind_bio_colissithon(idbio,idbio_conjoint, colissithon_url_port)
     #creation du pere du conjoint
     idbio_pere_conjoint=services.create_bio_minibio(prenom_pere_conjoint, nom_famille_pere_conjoint, None, None,  colissithon_url_port)
@@ -85,9 +88,8 @@ def process_94A():
     services.bind_bio_colissithon(idbio,idbio_mere_conjoint, colissithon_url_port)
 
     #parcour des destinations pour les envoyer dans file kafka travelthon
-    for i in range(len(voyage_json)):
-        destination=voyage_json[i]['pays']
-        producers.fill_travelthon_kafka(destination,idbio,travelthon_in,producer)
+    voyages_in_travelthon(voyage_json,idbio,travelthon_in,producer)
+    voyages_in_travelthon(voyage_conjoint_json,idbio_conjoint,travelthon_in,producer)
 
     #envoi de la bio dans googlethon
     producers.fill_googlethon_kafka(nomfamille,prenom, idbio,googlethon_in,producer)
@@ -96,6 +98,12 @@ def process_94A():
     generators.pictures_generator(pictures_directory, idbio, producer, comparathon_in)
     return idbio
 
+
+def voyages_in_travelthon(voyage_json,idbio,travelthon_in,producer):
+    #parcour des destinations pour les envoyer dans file kafka travelthon
+    for i in range(len(voyage_json)):
+        destination=voyage_json[i]['pays']
+        producers.fill_travelthon_kafka(destination,idbio,travelthon_in,producer)
 
 if __name__ == '__main__':
     app.run(host=ip, port=housthon_port)
